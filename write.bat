b1724a963053d63479abdbc0853cbb26cc161cfe
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-12 08:44:34 -0600

    Update write.bat

--- write.bat
+++ write.bat
@@ -1,4 +1,9 @@
 @ECHO OFF
+for %%X in (vasm6502.exe) do (set FOUND=%%~$PATH:X)
+if defined FOUND goto :compile
+ECHO vasm6502 not in PATH, aborting...
+EXIT
+:compile
 ECHO Compiling...
 vasm6502 -oldstyle -dotdir -esc -Fbin boot.s
 ECHO Writing...
