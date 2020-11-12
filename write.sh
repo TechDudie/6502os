c38f7ac0fcdecb27e0d41a013a24380ab47af3d4
TechDudie <73961295+TechDudie@users.noreply.github.com>
2020-11-12 09:20:31 -0600

    Update write.sh

--- write.sh
+++ write.sh
@@ -1,6 +1,14 @@
 printf "%s\n" "Compiling..."
+if ! [ -x "$(command -v vasm6502_oldstyle)" ]; then
+  echo 'Error: vasm6502_oldstyle is not installed.' >&2
+  exit 1
+fi
 vasm6502_oldstyle -dotdir -esc -Fbin boot.s
 printf "%s\n" "Writing..."
+if ! [ -x "$(command -v minipro)" ]; then
+  echo 'Error: minipro is not installed.' >&2
+  exit 1
+fi
 minipro -p AT28C256 -w a.out
 printf "%s\n" "Cleaning up..."
 del a.out
